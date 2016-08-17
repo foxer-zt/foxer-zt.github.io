b41deea2516f3c3003886062bb4a41b1f282d6e0
Irishdash <i@irishdash.tk>
2016-08-17 21:39:41 +0300

    Initial

--- js/application.js
+++ js/application.js
@@ -1,10 +1,9 @@
-angular.module('mooshApp', []).service('mooshProvider', ['$http', function ($http) {
+angular.module('mooshApp', ['ngRoute']).service('mooshProvider', ['$http', function ($http) {
     return {
         getAll: function() {
             return $http.get('https://foxer-zt.github.io/moosh.json');
         }
     }
 }]).config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/moosh', {controller: 'menuCtrl'});
-	$routeProvider.when('/moosh:videoIndex', {controller: 'menuCtrl'});
+	$routeProvider.when('/play/:moosh/:videoIndex', {controller: 'menuCtrl'});
 }]);
