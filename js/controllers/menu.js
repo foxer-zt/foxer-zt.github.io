a61bda0a8e25ef61dbdc08ee9fa6db1a9c002650
Irishdash <i@irishdash.tk>
2016-08-17 21:48:08 +0300

    Initial

--- js/controllers/menu.js
+++ js/controllers/menu.js
@@ -20,7 +20,7 @@ angular.module('mooshApp').controller('menuCtrl', ['$scope','mooshProvider', '$r
             $scope.selected = index;
         };
         $scope.selectedVideo = '';
-		init(scope.mooshs);
+		init($scope.mooshs);
         $scope.updateVideo = function(videoId) {
             $scope.selectedVideo = videoId;
             if(player) {
