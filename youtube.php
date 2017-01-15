f2db81696afa7c46906a1ff83b7253bb15e4a161
foxer-zt <foxer_zt@yahoo.com>
2017-01-15 16:09:43 +0200

    Update youtube.php

--- youtube.php
+++ youtube.php
@@ -14,21 +14,21 @@ if (!file_exists(__DIR__ . '/vendor/autoload.php')) {
 
 require_once __DIR__ . '/vendor/autoload.php';
 
-$htmlBody = <<<END
-<form method="GET">
-  <div>
-    Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
-  </div>
-  <div>
-    Max Results: <input type="number" id="maxResults" name="maxResults" min="1" max="50" step="1" value="25">
-  </div>
-  <input type="submit" value="Search">
-</form>
-END;
+// $htmlBody = <<<END
+// <form method="GET">
+//   <div>
+//     Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
+//   </div>
+//   <div>
+//     Max Results: <input type="number" id="maxResults" name="maxResults" min="1" max="50" step="1" value="25">
+//   </div>
+//   <input type="submit" value="Search">
+// </form>
+// END;
 
 // This code will execute if the user entered a search query in the form
 // and submitted the form. Otherwise, the page displays the form above.
-if (isset($_GET['q']) && isset($_GET['maxResults'])) {
+if (isset($_GET['q'])) {
   /*
    * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
    * {{ Google Cloud Console }} <{{ https://cloud.google.com/console }}>
@@ -44,25 +44,27 @@ if (isset($_GET['q']) && isset($_GET['maxResults'])) {
 
   $htmlBody = '';
   try {
-
+    
     // Call the search.list method to retrieve results matching the specified
     // query term.
     $searchResponse = $youtube->search->listSearch('id,snippet', array(
       'q' => $_GET['q'],
-      'maxResults' => $_GET['maxResults'],
+      'maxResults' => isset($_GET['maxResults']) ? $_GET['maxResults'] : 10,
     ));
 
     $videos = '';
     $channels = '';
     $playlists = '';
-
+    
     // Add each result to the appropriate list, and then display the lists of
     // matching videos, channels, and playlists.
+    $videoIds = [];
     foreach ($searchResponse['items'] as $searchResult) {
       switch ($searchResult['id']['kind']) {
         case 'youtube#video':
           $videos .= sprintf('<li>%s (%s)</li>',
               $searchResult['snippet']['title'], $searchResult['id']['videoId']);
+              $videoIds[] = $searchResult['id']['videoId'];
           break;
         case 'youtube#channel':
           $channels .= sprintf('<li>%s (%s)</li>',
@@ -74,26 +76,27 @@ if (isset($_GET['q']) && isset($_GET['maxResults'])) {
           break;
       }
     }
+    echo json_encode($videoIds);
 
-    $htmlBody .= <<<END
-    <h3>Videos</h3>
-    <ul>$videos</ul>
-    <h3>Channels</h3>
-    <ul>$channels</ul>
-    <h3>Playlists</h3>
-    <ul>$playlists</ul>
-END;
-  } catch (Google_Service_Exception $e) {
-    $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
-      htmlspecialchars($e->getMessage()));
-  } catch (Google_Exception $e) {
-    $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
-      htmlspecialchars($e->getMessage()));
-  }
-}
+//     $htmlBody .= <<<END
+//     <h3>Videos</h3>
+//     <ul>$videos</ul>
+//     <h3>Channels</h3>
+//     <ul>$channels</ul>
+//     <h3>Playlists</h3>
+//     <ul>$playlists</ul>
+// END;
+//   } catch (Google_Service_Exception $e) {
+//     $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
+//       htmlspecialchars($e->getMessage()));
+//   } catch (Google_Exception $e) {
+//     $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
+//       htmlspecialchars($e->getMessage()));
+//   }
+// }
 ?>
 
-<!doctype html>
+<!-- <!doctype html>
 <html>
   <head>
     <title>YouTube Search</title>
@@ -101,4 +104,4 @@ END;
   <body>
     <?=$htmlBody?>
   </body>
-</html>
+</html> -->
