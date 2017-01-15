642ef74d70e04ba7e3c133661571a2692d1cece9
foxer-zt <foxer_zt@yahoo.com>
2017-01-15 16:40:50 +0200

    Update vk.php

--- vk.php
+++ vk.php
@@ -7,7 +7,14 @@ $confirmation_token = 'f3de3c4e';
 $token = '1042eda5d74788e33e9d30a26392b333669169050edd86f60181752309c1bba4577bc7ec87c32a2645f11'; 
 $data = json_decode(file_get_contents('php://input')); 
 $commands = [
-  '!cat' => 'cat',
+  '!cat' => [
+    'function' =>'cat',
+    'hint' => '!cat'
+  ],
+  '!youtube' => [
+    'function' => 'youtube',
+    'hint' => '!youtube поисковый_запрос'
+  ]
 ];
 
 
@@ -21,9 +28,9 @@ switch ($data->type) {
     $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&v=5.0")); 
     $user_name = $user_info->response[0]->first_name;
     $message = "Привет, $user_name!\n Да прибудет с тобой сила!\nСписок доступных комманд:\n" . implode(', ', array_keys($commands));
-    foreach($commands as $command => $function) {
-      if (strpos($data->object->body, $command) !== false && function_exists($function)) {
-        $message = $function();
+    foreach($commands as $command => $data) {
+      if (strpos($data->object->body, $command) !== false && function_exists($data['function'])) {
+        $message = $data['function']($data->object->body);
       }
     }
     $request_params = array( 
@@ -39,7 +46,21 @@ switch ($data->type) {
   break; 
 }
 
-function cat()
+function youtube($text) 
+{
+    preg_match('@!youtube\s(.*)@', $text, $matches);
+    $matches = array_filter($matches);
+    if (!isset($matches[1]) ) {
+      return "Введите запрос. Например: !youtube котики";
+    } else {
+      $youtubeManager = 'https://irishdash.herokuapp.com/youtube.php?q=' . $matches[1];
+      $videoIds = json_decode(file_get_contents($youtubeManager), true);
+      $randomId = array_rand($videoIds);
+      return "По вашему запросу '{$matches[1]}' мы нашли:\nhttps://www.youtube.com/watch?v=" . $videoIds[$randomId];
+    }
+}
+
+function cat($text)
 {
     $catApiUrl = 'http://thecatapi.com/api/images/get?format=xml&api_key=MTUwMjE2';
     $xml = simplexml_load_string(file_get_contents($catApiUrl), "SimpleXMLElement", LIBXML_NOCDATA);
