25dca465584224e6f8380c2bfc0e9d54882fdc40
foxer-zt <foxer_zt@yahoo.com>
2017-01-15 14:34:31 +0200

    Update vk.php

--- vk.php
+++ vk.php
@@ -6,6 +6,8 @@ if (!isset($_REQUEST)) {
 $confirmation_token = 'f3de3c4e'; 
 $token = '1042eda5d74788e33e9d30a26392b333669169050edd86f60181752309c1bba4577bc7ec87c32a2645f11'; 
 $data = json_decode(file_get_contents('php://input')); 
+$commands = ['!cat' => 'cat'];
+
 
 switch ($data->type) { 
   case 'confirmation': 
@@ -16,12 +18,14 @@ switch ($data->type) {
     $user_id = $data->object->user_id; 
     $user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&v=5.0")); 
     $user_name = $user_info->response[0]->first_name;
-    $catApiUrl = 'http://thecatapi.com/api/images/get?format=xml';
-    $xml = simplexml_load_string(file_get_contents($catApiUrl), "SimpleXMLElement", LIBXML_NOCDATA);
-    $response = json_decode(json_encode($xml), true);
-    $catUrl = $response['data']['images']['image']['url'];
+    $message = "Привет, $user_name!\n Да прибудет с тобой сила!\nСписок доступных комманд:\n" . implode(', ' array_keys($commands));
+    foreach($commands as $command => $function) {
+      if (strpos($data->object->body, $command) !== false && function_exists($function)) {
+        $message = $function();
+      }
+    }
     $request_params = array( 
-      'message' => "Привет, {$user_name}!\nМы обязательно тебе ответим, ну а пока держи котика :3\n$catUrl", 
+      'message' => $message,
       'user_id' => $user_id, 
       'access_token' => $token, 
       'v' => '5.0' 
@@ -31,4 +35,12 @@ switch ($data->type) {
   file_get_contents('https://api.vk.com/method/messages.send?'. $get_params); 
   echo('ok'); 
   break; 
-} 
+}
+
+function cat()
+{
+    $catApiUrl = 'http://thecatapi.com/api/images/get?format=xml';
+    $xml = simplexml_load_string(file_get_contents($catApiUrl), "SimpleXMLElement", LIBXML_NOCDATA);
+    $response = json_decode(json_encode($xml), true);
+    return "Держи котика, няша :3\n" . $response['data']['images']['image']['url'];
+}
