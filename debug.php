903b2d9a9a538ce89e15b6a6aef0db179295e025
foxer-zt <foxer_zt@yahoo.com>
2017-01-15 14:06:56 +0200

    Update debug.php

--- debug.php
+++ debug.php
@@ -1,3 +1,7 @@
 <?php
-$carApiUrl = 'http://thecatapi.com/api/images/get';
-echo "<img src='$carApiUrl'>";
+$catApiUrl = 'http://thecatapi.com/api/images/get?format=xml';
+$xml = simplexml_load_string(file_get_contents($catApiUrl), "SimpleXMLElement", LIBXML_NOCDATA);
+$response = json_decode(json_encode($xml), true);
+var_dump($response);
+
+//echo "<img src='$carApiUrl'>";
