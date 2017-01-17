acce806fd94aa4764e6b490a0a3ef7523fa9625d
foxer-zt <foxer_zt@yahoo.com>
2017-01-17 23:22:29 +0200

    Update vk.php

--- vk.php
+++ vk.php
@@ -50,7 +50,8 @@ function mooshTube($text)
   preg_match('@!mooshTube\s(.*)\s(.*?)\s(.*)@', $text, $matches);
   $matches = array_filter($matches);
   if (!isset($matches[2]) || !isset($matches[3])) {
-    return "Введите запрос вида !mooshTube add mouse_name video_id\nНапример: !mooshTube add Irishdash H9HofYb_-kY";
+    return print_r($matches, true);
+    //return "Введите запрос вида !mooshTube add mouse_name video_id\nНапример: !mooshTube add Irishdash H9HofYb_-kY";
   }
   $videoStorage = file_get_contents(getLog('video'));
   if (!json_decode($videoStorage)) {
