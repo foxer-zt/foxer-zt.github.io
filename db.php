2822f5d1dee185190fb8eefe59178c679db50e77
foxer-zt <foxer_zt@yahoo.com>
2017-02-14 00:44:14 +0200

    Update db.php

--- db.php
+++ db.php
@@ -3,20 +3,20 @@ $host = $_ENV['DB_HOST'];
 $dbname = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
 $password = $_ENV['DB_PASSWORD'];
-echo "mysql:host=host;dbname=dbname", $user, $pass;
+
 try {
-  $dbh = new PDO("mysql:host=host;dbname=dbname", $user, $pass, 
-               array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-                die(json_encode(array('outcome' => true)));
+  $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass); 
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);             
+  $photoIds = array_filter(explode(';', file_get_contents('https://poacher-knock-48467.bitballoon.com/photoids.txt')));
+  foreach ($photoIds as &$id) {
+    $id = "($id)";
+  }
+  $query ='INSERT INTO photoIds VALUES ' . implode(', ', $photoIds);
+  $dbh->exec($query);
+  echo "New record created successfully";
+  
 } catch(PDOException $ex){
     die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
 }
 
-//$photoIds = array_filter(explode(';', file_get_contents('https://poacher-knock-48467.bitballoon.com/photoids.txt')));
-//foreach ($photoIds as &$id) {
-//  $id = "($id)";
-//}
-//$query ='INSERT INTO photoIds VALUES ' . implode(', ', $photoIds);
-$query ='INSERT INTO photoIds VALUES (3), (4), (5), (6), (7), (23)';
-$dbh->exec($query);
-echo "New record created successfully";
+
