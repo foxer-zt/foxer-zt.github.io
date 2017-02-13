c3a0987daaf4aad00d42c35377b02aa894c2b149
foxer-zt <foxer_zt@yahoo.com>
2017-02-14 00:30:11 +0200

    Create db.php

---
+++ db.php
@@ -0,0 +1,12 @@
+<?php
+$host = $_ENV['DB_HOST'];
+$dbname = $_ENV['DB_NAME'];
+$user = $_ENV['DB_USER'];
+$password = $_ENV['DB_PASSWORD'];
+$dbh = new PDO("mysql:host=host;dbname=dbname", $user, $pass);
+
+$photoIds = array_filter(explode(';', file_get_contents('https://poacher-knock-48467.bitballoon.com/photoids.txt')));
+foreach ($photoIds as &$id) {
+  $id = "($id)";
+}
+$dbh->query('INSERT INTO photoIds VALUES ' . implode(', ', $photoIds));
