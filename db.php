16c3b5cad44e11b0c9b7b2ea359f75c9e32eb31f
foxer-zt <foxer_zt@yahoo.com>
2017-02-14 00:39:03 +0200

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,13 @@ $host = $_ENV['DB_HOST'];
 $dbname = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
 $password = $_ENV['DB_PASSWORD'];
-$dbh = new PDO("mysql:host=host;dbname=dbname", $user, $pass);
+try {
+  $dbh = new PDO("mysql:host=host;dbname=dbname", $user, $pass, 
+               array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+                die(json_encode(array('outcome' => true)));
+} catch(PDOException $ex){
+    die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+}
 
 //$photoIds = array_filter(explode(';', file_get_contents('https://poacher-knock-48467.bitballoon.com/photoids.txt')));
 //foreach ($photoIds as &$id) {
