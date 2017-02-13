51803f5541322b22ad8f46298d516030cbbc6bcf
foxer-zt <foxer_zt@yahoo.com>
2017-02-14 01:48:48 +0200

    Update ssgv.php

--- ssgv.php
+++ ssgv.php
@@ -11,13 +11,12 @@ try {
   $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $password); 
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   $data = $dbh->query('SELECT * FROM photoIds ORDER BY RAND() LIMIT 1')->fetchAll(PDO::FETCH_ASSOC);
-  $results = $dbh->fetchAll(PDO::FETCH_ASSOC);
   var_dump($dbh->queryString);
-  foreach($results as $row) {
+  foreach($data as $row) {
     var_dump($row);
     var_dump($_GET['random']);
   }
-  echo $results[0][$_GET['random']];
+  echo $data[0][$_GET['random']];
   $dbh = null;
 } catch(PDOException $e){
     die($query . "<br>" . $e->getMessage());
