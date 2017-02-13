70e5244546280531e5affd0f643052e460263604
foxer-zt <foxer_zt@yahoo.com>
2017-02-14 01:53:24 +0200

    Update ssgv.php

--- ssgv.php
+++ ssgv.php
@@ -1,8 +1,10 @@
+<meta charset="windows-1252">
 <?php
 $tables = ['quote', 'photoId'];
 if (!isset($_GET['random']) || !in_array($_GET['random'], $tables)) {
   die('Missed "random" key. Available values: ' . implode(', ', $tables));
 }
+$table = $_GET['random'] . 's';
 $host = $_ENV['DB_HOST'];
 $dbname = $_ENV['DB_NAME'];
 $user = $_ENV['DB_USER'];
@@ -10,12 +12,7 @@ $password = $_ENV['DB_PASSWORD'];
 try {
   $dbh = new PDO("mysql:host=$host;dbname=$dbname", $user, $password); 
   $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $data = $dbh->query('SELECT * FROM photoIds ORDER BY RAND() LIMIT 1')->fetchAll(PDO::FETCH_ASSOC);
-  var_dump($dbh->queryString);
-  foreach($data as $row) {
-    var_dump($row);
-    var_dump($_GET['random']);
-  }
+  $data = $dbh->query("SELECT * FROM $table ORDER BY RAND() LIMIT 1")->fetchAll(PDO::FETCH_ASSOC);  
   echo $data[0][$_GET['random']];
   $dbh = null;
 } catch(PDOException $e){
