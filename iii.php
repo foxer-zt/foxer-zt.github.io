c2a950d398f67ee48104580d52584b7148c0e5ec
foxer-zt <foxer_zt@yahoo.com>
2017-02-14 02:29:11 +0200

    Update iii.php

--- iii.php
+++ iii.php
@@ -1,49 +1,54 @@
 <?php
-class ModIiiRu
+//создаем функции, которые будут отсылать боту все необходимые данные
+class XORFUNC
 {
-    const IiiID = 'a3c32fd5-e666-48a7-97fc-eb4630629760';
-    public function cmd($cmd)
-    {
-	      global $cook;
-        $data = json_encode(array($cook, $cmd));
-        $data = self::sendMessage(self::xorKey($data, false));
-        $data = json_decode($data);
-        $text = $data->result->text->value;
-        return $text;
-        //$this->say($text);
-    }
- private static function sendMessage($send){
-        $url='http://iii.ru/api/2.0/json/Chat.request';
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_TIMEOUT, 3);
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $send);
-        $res = curl_exec($ch);
-        if (!$res)
-            return false;
-        curl_close($ch);
-        return self::xorKey($res, true);
-    }
-    static function xorKey($res, $decode)
-    {
-        $key = "some very-very long string without any non-latin characters due to different string representations inside of variable programming languages";
-        $keylen = strlen($key);
-        $res = $decode ? base64_decode($res) : base64_encode($res);
-        $strlen = strlen($res);
-        $i = 0;
-        $result = '';
-        while ($i<$strlen)
-        {
-            $buf = $res[$i] ^ $key[$i % $keylen];
-            $result .= $buf;
-            $i++;
-        }
-        return $decode ? base64_decode($result) : base64_encode($result);
-    }
+ public static function XOR_encrypt($message, $key)
+ {
+ $ml = strlen($message);
+ $kl = strlen($key);
+ $newmsg = "";
+
+ for ($i = 0; $i < $ml; $i++) {
+ $newmsg = $newmsg . ($message[$i] ^ $key[$i % $kl]);
+ }
+
+ return base64_encode($newmsg);
+ }
+
+ public static function XOR_decrypt($encrypted_message, $key)
+ {
+ $msg = base64_decode($encrypted_message);
+ $ml = strlen($msg);
+ $kl = strlen($key);
+ $newmsg = "";
+
+ for ($i = 0; $i < $ml; $i++) {
+ $newmsg = $newmsg . ($msg[$i] ^ $key[$i % $kl]);
+ }
+ return $newmsg;
+ }
 }
-$mod = new ModIiiRu();
-echo $mod->cmd($_GET['text']);
+
+//настройки бота, нам понадобиться лишь один параметр - id бота, его можно узнать, просто наведя мышку на своего бота или чужого бота в сервисе.
+$config['botid'] = "e8ae75f7-d931-4300-974c-8fa5a69142d5";
+//остальные настройки получаем автоматически
+$config['url'] = 'http://' . $_SERVER['HTTP_HOST'];
+$config['key'] = "some very-very long string without any non-latin characters due to different string representations inside of variable programming languages";
+
+$vopros = "Добрый день!";
+$whattosend = '["' . $session . '","' . urldecode($vopros) . '"]';
+$hashed = XORFUNC::XOR_encrypt(base64_encode($whattosend), $config['key']);
+$myCurl = curl_init();
+curl_setopt_array($myCurl, array(
+ CURLOPT_URL => 'http://iii.ru/api/2.0/json/Chat.request',
+ CURLOPT_RETURNTRANSFER => true,
+ CURLOPT_POST => true,
+ CURLOPT_POSTFIELDS => $hashed,
+));
+$response = curl_exec($myCurl);
+curl_close($myCurl);
+
+//получаем ответ от бота на вопрос
+$answer = json_decode(base64_decode(XORFUNC::XOR_decrypt($response, $config['key'])));
+$otvet = $answer->result->text->value;
+echo $otvet;
