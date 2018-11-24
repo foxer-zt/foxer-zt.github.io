cd4df6067b961f7217d82dd10dfc1db2bd1339f9
foxer-zt <foxer_zt@yahoo.com>
2018-11-24 20:08:53 +0200

    Update tbot.php

--- tbot.php
+++ tbot.php
@@ -139,7 +139,7 @@ function processMessage($message) {
 }
 
 
-define('WEBHOOK_URL', 'https://my-site.example.com/secret-path-for-webhooks/');
+define('WEBHOOK_URL', 'http://irishdash.herokuapp.com/tbot.php');
 
 if (php_sapi_name() == 'cli') {
   // if run from console, set or delete webhook
